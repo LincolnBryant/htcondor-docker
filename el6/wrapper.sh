65300cae1a7fb59f49522e736041123c74879c2c
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2016-12-16 15:07:33 -0600

    OSG_WN_TMP is wrong here. the condor sandbox is the correct location

--- el6/wrapper.sh
+++ el6/wrapper.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/current/init/bash
-export OSG_WN_TMP=/scratch
 export OSG_SITE_NAME="UC3"
 export OSG_SQUID_LOCATION="uct2-squid.mwt2.org:3128"
 exec "$@"
