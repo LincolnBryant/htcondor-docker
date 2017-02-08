dd1003d02d4b6ea5b926363aafd8ee5c317ac755
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2017-02-08 16:31:12 -0600

    we found the dev but didnt actually use it. oops

--- el6/set-hostname.sh
+++ el6/set-hostname.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 DEV=$(ip -4 route show default | cut -d" " -f5)
-IP4=$(ip -4 address show dev eno1 | grep inet | tr " " / | cut -d/ -f6)
+IP4=$(ip -4 address show dev $DEV | grep inet | tr " " / | cut -d/ -f6)
 FQDN=$(hostname)
 HOSTNAME=$(hostname -s)
 
